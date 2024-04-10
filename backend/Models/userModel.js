6c69d683939f272feb223ecb585c729fe67d3baf
Aman <aman.mishra.2005@gmail.com>
2024-04-10 15:40:30 +0530

    Modified Data and Converted to json

--- backend/Models/userModel.js
+++ backend/Models/userModel.js
@@ -1,1 +1,36 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.model({
+  name: {
+    type: String,
+    required: [true, "A user must have a Name"],
+  },
+  email: {
+    type: String,
+    required: [true, "A user must have an Email"],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, "Please provide a valid email"],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, "Please Provide the password"],
+    minlength: 8,
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "Please Confirm your password"],
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: "Passwords are not same",
+    },
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,20 @@
+const fs = require("fs");
 const express = require("express");
 const morgan = require("morgan");
+
 const app = express();
+const exercises = JSON.parse(
+  fs.readFileSync(`${__dirname}/Data/data/CompleteData/completedata.json`)
+);
+app.get("/api/v1/exercises", (re, res) => {
+  res.status(200).json({
+    status: "Sucess",
+    results: exercises.length,
+    data: {
+      exercises,
+    },
+  });
+});
 
 app.use(morgan("dev"));
 module.exports = app;
