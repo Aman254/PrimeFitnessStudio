e65a88cf8de478850e7fbc0fe8d06f62a123867a
Aman <aman.mishra.2005@gmail.com>
2024-04-14 12:55:27 +0530

    Error Handling

--- backend/Controllers/exerciseController.js
+++ backend/Controllers/exerciseController.js
@@ -1,7 +1,8 @@
 const Exercise = require("./../Models/exerciseModel");
 const ApiFeatures = require("./../utils/apiFeatures");
+const catchAsync = require("./../utils/catchAsync");
 
-exports.getAllExercises = async (req, res) => {
+exports.getAllExercises = catchAsync(async (req, res, next) => {
   const features = new ApiFeatures(Exercise.find(), req.query)
     .filter()
     .limitFields()
@@ -10,18 +11,12 @@ exports.getAllExercises = async (req, res) => {
   const exercises = await features.query;
 
   //Send Response
-  try {
-    res.status(200).json({
-      results: exercises.length,
-      status: "Sucess",
-      data: {
-        exercises,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: "Failed",
-      message: err,
-    });
-  }
-};
+
+  res.status(200).json({
+    results: exercises.length,
+    status: "Sucess",
+    data: {
+      exercises,
+    },
+  });
+});
--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const morgan = require("morgan");
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./Controllers/errorController");
 const exerciseRouter = require("./Routes/exerciseRoutes");
 
 const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
-app.use("/api/v1/exercises", exerciseRouter);
 
+//Routes
+app.use("/api/v1/exercises", exerciseRouter);
+// app.use("/api/v1/users")
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this Server`, 404));
+});
+
+app.use(globalErrorHandler);
 module.exports = app;
