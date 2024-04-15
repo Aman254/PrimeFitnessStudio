c006ea66b479c9c35dce57f6553f435fb39325f2
Aman <aman.mishra.2005@gmail.com>
2024-04-15 15:08:13 +0530

    Added Headers to exercise Page

--- frontend/src/pages/exercises.jsx
+++ frontend/src/pages/exercises.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { fetchExercises } from "../Services/api";
-
+import Navbar from "../components/Navbar";
+import image from "../Assets/gymImages/gymImg4.jpg";
 const Exercises = () => {
   const [Exercise, setExercise] = useState([]);
   useEffect(() => {
@@ -11,11 +12,40 @@ const Exercises = () => {
       .catch((error) => console.log(error));
   }, []);
 
-  console.log(Exercise);
+  //   console.log(Exercise);
   return (
-    <div className="text-white">
-      <h1>Aman</h1>
-    </div>
+    <>
+      <div className="">
+        <Navbar />
+      </div>
+      <div
+        className="md:flex flex-row justify-between w-full items-center text-white h-screen 
+      md:mt-0 mt-40 gap-40"
+      >
+        <div className="md:w-1/2 w-full">
+          <div className="md:p-4 p-2">
+            <span className="text-xl font-poppins font-medium text-yellow-400">
+              Prime Fitness Studio
+            </span>
+          </div>
+          <div className="p-4">
+            <h1 className="text-6xl font-oswald font">
+              Sweat, Smile and Repeat
+            </h1>
+          </div>
+          <div className="p-2">
+            <span>
+              Checkout the most Effective exercise personalized for you
+            </span>
+          </div>
+        </div>
+        <div className="">
+          <img src={image} alt="" className="md:h-screen h-1/2" />
+        </div>
+      </div>
+
+      <div className="bg-white w-full h-screen"></div>
+    </>
   );
 };
 
