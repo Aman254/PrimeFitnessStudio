da63f3f0d27d2bdbf32cf0a7d7077fc55996b055
Aman <aman.mishra.2005@gmail.com>
2024-04-17 12:11:10 +0530

    Modified few files

---
+++ frontend/src/components/Avatar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Avatar = () => {
+  return (
+    <div>
+      <div className="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+        <svg
+          className="absolute w-12 h-12 text-gray-400 -left-1"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+      </div>
+    </div>
+  );
+};
+
+export default Avatar;
